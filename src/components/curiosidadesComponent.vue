f1ffbb21c9fa3d6d68404d0eb49bb66085262339
Manteca-Daniel <danielmanteca.dmg1@gmail.com>
2024-12-10 16:09:26 +0100

    Proyecto finalizado

--- src/components/curiosidadesComponent.vue
+++ src/components/curiosidadesComponent.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
-        <h1>CURIOSIDADES DE SHINGEKI NO KYOJIN</h1>
-        <div v-for="curiosidad in curiosidades" :key="index">
-            <h3>{{ curiosidad.titulo }}</h3>
-            <p>{{ curiosidad.descripcion }}</p>
+    <div class="curiosidades-container">
+        <h1 class="titulo-principal">Curiosidades de Shingeki no Kyojin</h1>
+        <div class="curiosidad" v-for="(curiosidad, index) in curiosidades" :key="index">
+            <h3 class="curiosidad-titulo">{{ curiosidad.titulo }}</h3>
+            <p class="curiosidad-descripcion">{{ curiosidad.descripcion }}</p>
+            <img v-if="curiosidad.imagen" :src="curiosidad.imagen"
+                :alt="`Curiosidad especial ${index + 1}`" class="curiosidad-imagen" />
         </div>
     </div>
 </template>
@@ -21,9 +23,7 @@ const fetchCuriosidades = async () => {
         if (response.status !== 200) {
             throw new Error('Error al cargar las curiosidades');
         }
-        const data = response.data;
-        curiosidades.value = data.curiosidades;
-        console.log(curiosidades.value)
+        curiosidades.value = response.data.curiosidades;
     } catch (error) {
         console.error('Error al obtener los datos:', error);
     }
